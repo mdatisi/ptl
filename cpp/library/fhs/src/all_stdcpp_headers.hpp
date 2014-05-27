5739b53204c1edca2f6f6e1c5fd9210f9046095a
Laurent Watteau <contact@laurent-watteau.com>
2014-05-27 07:18:45 +0200

    Created file

---
+++ cpp/library/fhs/src/all_stdcpp_headers.hpp
@@ -0,0 +1,86 @@
+//------------------------------------------------------------------------------
+#ifndef MD_ALL_STDCPP_HEADERS_
+#define MD_ALL_STDCPP_HEADERS_
+
+//ISO/IEC 14882:2011
+#include <cstdlib>            //C library - General purpose utilities
+#include <csignal>            //C library - Signal handling
+#include <csetjmp>            //C library - Non local jumps
+#include <cstdarg>            //C library - Variable arguments handling
+#include <typeinfo>           //Type information
+#include <typeindex>          //(C++11) Type index
+#include <type_traits>        //(C++11) Type traits
+#include <bitset>             //Bitset
+#include <functional>         //Function object
+#include <utility>            //Various utility components
+#include <ctime>              //C library - Time/date utilities
+#include <chrono>             //(C++11) Time/Date utilities
+#include <cstddef>            //C library - C standard definitions
+#include <initializer_list>   //(C++11) Initializer list
+#include <tuple>              //(C++11) Tuple handling
+#include <new>                //Dynamic memory
+#include <memory>             //Memory elements
+#include <scoped_allocator>   //(C++11) Nested allocator class
+#include <climits>            //C library - Sizes of integral types
+#include <cfloat>             //C library - Properties of Floating-point types
+#include <cstdint>            //(C++11) C library - Integer types
+#include <cinttypes>          //(C++11) C library - Integer types
+#include <limits>             //Numeric limits
+#include <exception>          //Exception handling utilities
+#include <stdexcept>          //Standard exception objects
+#include <cassert>            //C library - Diagnostics library
+#include <system_error>       //(C++11) System errors (platform-specific)
+#include <cerrno>             //C library - Last error handling
+#include <cctype>             //C library - Character handling functions
+#include <cwctype>            //C library - Wide character type 
+#include <cstring>            //C library - strings
+#include <cwchar>             //C library - Wide characters
+//#include <cuchar>           //(C++11) C library - Unicode characters
+#include <string>             //Strings
+#include <array>              //(C++11) Arrays
+#include <vector>             //Vectors
+#include <deque>              //Double-ended queues
+#include <list>               //Lists
+#include <forward_list>       //(C++11) Forward lists
+#include <set>                //Sets and multisets 
+#include <map>                //Maps and multimaps 
+#include <unordered_set>      //(C++11) Unordered sets & multisets
+#include <unordered_map>      //(C++11) Unordered maps & multimaps
+#include <stack>              //Stacks
+#include <queue>              //Queues
+#include <algorithm>          //Algorithms that operate on containers
+#include <iterator>           //Container iterators
+#include <cmath>              //C library - Common mathematics functions
+#include <complex>            //Complex numbers library
+#include <valarray>           //Arrays of numeric values
+#include <random>             //(C++11) Random number generators & distributions
+#include <numeric>            //Numeric operations on values in containers
+#include <ratio>              //(C++11) Compile-time rational arithmetic
+#include <cfenv>              //(C++11) C library - Floating-point environment
+#include <iosfwd>             //Input/Output forward declarations
+#include <ios>                //Input/Output base classes
+#include <istream>            //Input stream classes
+#include <ostream>            //Output stream classes
+#include <iostream>           //Standard Input/Output streams library
+#include <fstream>            //File streams
+#include <sstream>            //String streams
+#include <iomanip>            //Input/Output manipulators
+#include <streambuf>          //Stream buffers
+#include <cstdio>             //C library - Input/Output operations
+#include <locale>             //Localization library
+#include <clocale>            //C library - Localization library
+//#include <codecvt>          //(C++11) Unicode conversion facets
+#include <regex>              //(C++11) Regular expressions library
+#include <atomic>             //(C++11) Atomic operations library
+#include <thread>             //(C++11) Threads
+#include <mutex>              //(C++11) Mutual exclusion primitives
+//#include <shared_mutex>     //(C++14) Shared mutual exclusion primitives
+#include <future>             //(C++11) Primitives for asynchronous computations
+#include <condition_variable> //(C++11) Thread waiting conditions
+#include <ciso646>            //(Empty) C <iso646.h> macros are C++ keywords
+#include <ccomplex>           //(C++11) Includes header <complex>
+#include <ctgmath>            //(C++11) Includes headers <complex> and <cmath>
+#include <cstdalign>          //(C++11) Defines one compatibility macro constant
+
+#endif //#ifndef MD_ALL_STDCPP_HEADERS_
+//------------------------------------------------------------------------------
