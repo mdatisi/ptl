1021e0e5041b4513a25049db984d999db0650e77
Laurent Watteau <lwatteau@datisi.fr>
2014-12-10 19:42:01 +0100

    Some modifications...

--- cpp/program/fhs/src/main.cpp
+++ cpp/program/fhs/src/main.cpp
@@ -1,12 +1,17 @@
+//Module
 #include "../include/class.hpp"
+
+//C++ Standard Library
+#include <iostream>
 
 int main()
 {
 try{
     MyClass::my_function();
-    return 1;
+    throw 0;
+    return 0;
 }
 catch(...) {
-
+    std::cerr << "Unspecified exception caught at main level" << std::endl;
 }
 }
