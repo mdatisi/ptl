ff08d91c8224fc6be42918f13a6171f14ad13d3d
Laurent Watteau <contact@laurent-watteau.com>
2015-08-09 01:21:42 +0200

    Added comment

--- cpp/patterns/visitor/action_1.cpp
+++ cpp/patterns/visitor/action_1.cpp
@@ -14,7 +14,7 @@ using namespace std;
 
 action_1::action_1() : visited_base::visitor(
 {
-    {elt_1::key, [](visited_base* p) 
+/*    {elt_1::key, [](visited_base* p) 
         {
             cout << "In ACTION_1::ELT_1 handler" << endl;
             elt_1* e = dynamic_cast<elt_1*>(p);
@@ -27,7 +27,7 @@ action_1::action_1() : visited_base::visitor(
             elt_2* e = dynamic_cast<elt_2*>(p);
             cout << e->method_of_elt_2() << endl;            
         }
-    },
+    },*/
     {"", [](visited_base* p)
         {
             cout << "In ACTION_1::ERROR handler" << endl;
@@ -42,6 +42,9 @@ action_1::action_1() : visited_base::visitor(
 void action_1::visit(visited_base* p)
 {
     cout << "key : " << p->unique_key() << endl;
+
+
+    //[TODO] Manage unknown or unsupported keys
     m_key_map[p->unique_key()](p);
 }
 
